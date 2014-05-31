49ee2aa713ccaf98d030ef99d97d1b7cbeb5f252
syoukore <thpss89386@gmail.com>
2014-05-31 17:33:43 +0800

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,45 +1,18 @@
+`include "defines.v"
+
 module alu(alu_function,alu_sel,alu_src1,alu_src2,shamt,alu_result,alu_zero,alu_result);
 
 /***********************************************************/
 
-/*boolean logic*/
-`define TRUE  1
-`define FALSE 0
-`define GROUND 32'h0000_0000
-//Define
-`define DATA_WIDTH 32
-`define OPERATE 5
-`define SHMAT 5
-
-`define CTRL_SLL   5'b00000
-`define CTRL_SRL   5'b00010
-
-`define CTRL_MULT  5'b01000
-`define CTRL_MULTU 5'b01001
-`define CTRL_DIV   5'b01010
-`define CTRL_DIVU  5'b01011
-
-`define CTRL_ADD   5'b10000
-`define CTRL_ADDU  5'b10001
-`define CTRL_SUB   5'b10010
-`define CTRL_SUBU  5'b10011
-`define CTRL_AND   5'b10100
-`define CTRL_OR    5'b10101
-`define CTRL_XOR   5'b10110
-`define CTRL_NOR   5'b10111
-
-`define CTRL_SLT   5'b11010
-`define CTRL_SLTU  5'b11011
-
 //Inputs
-input [`DATA_WIDTH-1:00]alu_src1;
-input [`DATA_WIDTH-1:00]alu_src2;
+input [`WORD_WIDTH-1:00]alu_src1;
+input [`WORD_WIDTH-1:00]alu_src2;
 input alu_sel;
-input [`OPERATE-1:00]alu_function;
-input [`SHMAT-1:00]shamt;
+input [`ALUFUNCT_WIDTH-1:00]alu_function;
+input [`SHAMT_WIDTH-1:00]shamt;
 //Outputs
 output alu_zero;
-output reg[`DATA_WIDTH-1:00]alu_result;
+output reg[`WORD_WIDTH-1:00]alu_result;
 
 
 /********************************************************/
@@ -50,64 +23,64 @@ assign alu_zero=(alu_src1==alu_src2)?1:0;
 always@(alu_function,alu_src1,alu_src2)begin
 	if(alu_sel==0)
 	case(alu_function)
-		`CTRL_SLL:begin
+		`ALUFUNCT_SLL:begin
 			alu_result=alu_src1<<shamt;
 			end
-		`CTRL_SRL:begin
+		`ALUFUNCT_SRL:begin
 			alu_result=alu_src1>>shamt;
 			end	
-	    `CTRL_ADD:begin
+	    `ALUFUNCT_ADD:begin
             alu_result=alu_src1+alu_src2;
              end
-		`CTRL_ADDU:begin
+		`ALUFUNCT_ADDU:begin
             alu_result=alu_src1+alu_src2;
              end
-        `CTRL_SUB:begin
+        `ALUFUNCT_SUB:begin
 		    alu_result=alu_src1-alu_src2;
 			end
-		`CTRL_SUBU:begin
+		`ALUFUNCT_SUBU:begin
 		    alu_result=alu_src1-alu_src2;
 			end
-		`CTRL_AND:begin
+		`ALUFUNCT_AND:begin
 			alu_result=alu_src1&alu_src2;
 			end
-		`CTRL_OR:begin
+		`ALUFUNCT_OR:begin
 			alu_result=alu_src1|alu_src2;
 			end
-		`CTRL_XOR:begin
+		`ALUFUNCT_XOR:begin
 			alu_result=alu_src1^alu_src2;
 			end
-		`CTRL_NOR:begin
+		`ALUFUNCT_NOR:begin
 			alu_result=~(alu_src1|alu_src2);
 			end
-		`CTRL_SLT:begin
-			alu_result=alu_src1<alu_src2?32'h0000_0001:32'h0000_0000;
+		`ALUFUNCT_SLT:begin
+			alu_result=alu_src1<alu_src2?`TRUE:`FALSE;
 			end
-		`CTRL_SLTU:begin
-			alu_result=alu_src1<alu_src2?32'h0000_0001:32'h0000_0000;
+		`ALUFUNCT_SLTU:begin
+			alu_result=alu_src1<alu_src2?`TRUE:`FALSE;
 			end
 		default:begin
-			alu_result=`GROUND;
+			alu_result=0;
 			$display("0Error in alu_EX!");
 			end
 	endcase
 	else if(alu_sel==1)
 		case(alu_function)
-			`CTRL_MULT:
+			`ALUFUNCT_MULT:
 				alu_result=alu_src1*alu_src2;
-			`CTRL_MULTU:
+			`ALUFUNCT_MULTU:
 				alu_result=alu_src1*alu_src2;
-			`CTRL_DIV:
+			`ALUFUNCT_DIV:
 				alu_result=alu_src1/alu_src2;
-			`CTRL_DIVU:
+			`ALUFUNCT_DIVU:
 				alu_result=alu_src1/alu_src2;
 			default:begin
-				alu_result=`GROUND;
+				alu_result=0;
 				$display("1Error in alu_EX!");
 			end
 		endcase
 	else begin
-		alu_result=`GROUND;
+		alu_result=0;
 		$display("xError in alu_EX!");
 	end
 end
