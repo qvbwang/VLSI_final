618d4873cf1e421eff0d4f435c5f0a87582b6167
syoukore <thpss89386@gmail.com>
2014-06-07 03:26:46 +0800

    Update prog_count.v
    bug fixed : Stall IF will lost one instruction

--- prog_count.v
+++ prog_count.v
@@ -2,29 +2,35 @@
 
 /*
 *[140604] add function: stall
+*[140607] bug fixed : Stall IF will lost one instruction
 */
 
 module prog_count(  clk, rst, 
-                    stall, pc_src, jumpaddr, nxtaddr);
+                    stall, pc_src, jumpaddr, nowaddr, nxtaddr);
     input clk, rst;
     input stall, pc_src;
     input [`WORD_WIDTH-1:0] jumpaddr;
-    output [`WORD_WIDTH-1:0] nxtaddr;
+    output [`WORD_WIDTH-1:0] nowaddr, nxtaddr;
     
-    reg [`WORD_WIDTH-1:0] nxtaddr;
+    reg [`WORD_WIDTH-1:0] nowaddr, nxtaddr;
     
     always@(posedge clk or posedge rst) begin
         if(rst) begin
+            nowaddr <= `PC_INIT;
             nxtaddr <= `PC_INIT;
         end
-        else if(stall)
+        else if(stall) begin
+            nowaddr <= nowaddr;
             nxtaddr <= nxtaddr;
+        end
         else
             case(pc_src)
                 `PC_JUMP: begin
+                    nowaddr <= jumpaddr;
                     nxtaddr <= jumpaddr + 4; // hard code!
                 end
                 /*`PC_INCRESE*/default: begin
+                    nowaddr <= nxtaddr;
                     nxtaddr <= nxtaddr + 4; // hard code!
                 end
             endcase
